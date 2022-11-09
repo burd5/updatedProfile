a5f7570c9c0f2d51082ee9a3ee6df40a120df700
burd5 <austinburdette5@gmail.com>
2022-11-09 15:19:38 -0700

    scrollspy edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 (function() {
   "use strict";
 
@@ -34,25 +36,42 @@
     el.addEventListener('scroll', listener)
   }
 
+
   /**
    * Navbar links active state on scroll
    */
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
+
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
       if (!section) return
       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
+        navbarlink.classList.add('.active')
       } else {
-        navbarlink.classList.remove('active')
+        navbarlink.classList.remove('.active')
       }
     })
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
+
+  /*const links = document.querySelectorAll('.nav-link');
+    
+  if (links.length) {
+    links.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        links.forEach((link) => {
+            link.classList.remove('active');
+        });
+        e.preventDefault();
+        link.classList.add('active');
+      });
+    });
+  }*/
 
   /**
    * Scrolls to an element with header offset
